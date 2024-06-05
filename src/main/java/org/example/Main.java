289d08bb7ab4633748b9d4a492b9e0886f51de09
f0ng33621 <94217638+f0ng33621@users.noreply.github.com>
2024-06-05 21:41:10 +0700

    Comment all test code in DBcustomerRepo(Online)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,10 @@ package org.example;
 
 import ui.UiRestaurant;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         var Open = new UiRestaurant(false);
 
         Open.start();
-
-
     }
 }
